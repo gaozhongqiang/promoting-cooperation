8c17c0fc193a1289af3ee094187a7ebe284a2097
gaozhongqiang <1225000209@qq.com>
2019-08-03 10:09:55 +0800

    基础文件

---
+++ identity.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gzq
+ * Date: 2019/8/3
+ * Time: 10:01
+ * 身份认证
+ */
+class Identity{
+    //身份认证信息驳回
+    public static function reject($_this,$promuser_id){
+
+    }
+}
